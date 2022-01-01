e506a1572aef771af7e81f9e654467411bb291ff
Michael March <s222074@student.uek.krakow.pl>
2022-01-01 23:52:47 +0100

    Make the row creation string more readable

--- data.js
+++ data.js
@@ -1,18 +1,25 @@
 const connection = new XMLHttpRequest;
 
 connection.addEventListener("load", e => {
-    if (connection.status === 200)
-    {
+    if (connection.status === 200) {
         const json = JSON.parse(connection.response);
         console.log(json)
 
-        for (let row of json)
-        {
-            var str = "<tr>" + "<td>" + row["first_name"] + "</td>" +
-            "<td>" + row["last_name"] + "</td>" + "<td>" + row["birth_date"] + "</td>" + "<td>" + row["gender"] + "</td>" + "<td>" + row["index_number"] + "<td>" + 
-            row["group"] + "</td>" + "<td>" + row["first_language"] + "</td>" + "<td>" + row["second_language"] + "</td>" + "<td>" + row["due"] + "</td>" +
-            "<td>" + row["university_dormitory"] + "</td>" + "</tr>";
-
+        for (let row of json) {
+            let str = `
+<tr>
+    <td>${row["first_name"]}</td>
+    <td>${row["last_name"]}</td>
+    <td>${row["birth_date"]}</td>
+    <td>${row["gender"]}</td>
+    <td>${row["index_number"]}</td>
+    <td>${row["group"]}</td>
+    <td>${row["first_language"]}</td>
+    <td>${row["second_language"]}</td>
+    <td>${row["due"]}</td>
+    <td>${row["university_dormitory"]}</td>
+</tr>
+`
             document.getElementById("target").innerHTML += str;
         }
     }
