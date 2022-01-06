8f62f4f073bc410bdfd13e9e2d5e299d4d28c21a
Michael March <s222074@student.uek.krakow.pl>
2022-01-06 15:34:12 +0100

    Prepare data for charts

--- data.js
+++ data.js
@@ -5,6 +5,15 @@ connection.addEventListener("load", e => {
         const json = JSON.parse(connection.response);
         console.log(json)
 
+        let personCount = {};
+        let foreignLanguagePercentage = {};
+        let namesStartingWithVowel = {
+            "Januray": {}, "Febuary": {}, "March": {},
+            "April": {}, "May": {}, "June": {}, "July": {},
+            "August": {}, "September": {}, "October": {},
+            "November": {}, "December": {}
+        };
+
         for (let row of json) {
             let str = `
 <tr>
@@ -21,13 +30,67 @@ connection.addEventListener("load", e => {
 </tr>
 `
             document.getElementById("target").innerHTML += str;
+
+            updateHashTable(personCount, row["group"])
+            updateHashTable(foreignLanguagePercentage, row["second_language"]);
+
+            let monthName = getMonthNameFromStringDate(row["birth_date"]);
+            let firstNameLetter = row["first_name"][0];
+
+            updateHashTable(namesStartingWithVowel[monthName], firstNameLetter, isVowel);
+
+            let lastNameLetter = row["last_name"][0];
+
+            updateHashTable(namesStartingWithVowel[monthName], lastNameLetter, isVowel);
         }
+        console.log(personCount);
+        console.log(foreignLanguagePercentage);
+        console.log(namesStartingWithVowel);
     }
 });
 
 connection.open("GET", "https://my.api.mockaroo.com/students_database.json?key=ab70dc60", true);
 connection.send();
 
+/*
+    Returns month name from a string date. Requries the date format to be:
+        * dd-mm-yyyy
+        * dd/mm/yyyy
+    Or any other separtor as long as month is always a 2-digit number and
+    starts at position 3 (counting from 0)
+*/
+let getMonthNameFromStringDate = (str_date) => {
+    // For substring first parameter is inclusive. but the second
+    // is exclusive: [3; 5)
+    let monthNumber = parseInt(str_date.substring(3, 5));
 
+    let monthNames = [
+        "Januray", "Febuary", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ]
 
+    return monthNames[monthNumber - 1];
+};
 
+/*
+    Returns true if c is a vowel and false otherwise.
+    Expects c to be string of length equal to 1
+*/
+let isVowel = (c) => {
+    c = c.toLowerCase();
+    return c == "a" || c == "e" || c == "i" || c == "o" || c == "u";
+}
+
+/*
+    Updates HashTable object for a given key
+    with a starting value 1 incremented in subsequent calls
+*/
+let updateHashTable = (hashTable, key, keyConstraint = (_key) => { return true; }) => {
+    if (keyConstraint(key)) {
+        if (!(key in hashTable)) {
+            hashTable[key] = 1;
+        } else {
+            hashTable[key]++;
+        }
+    }
+}
